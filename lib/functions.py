eea798dacc569984005c787b53194df64bfbfb37
mary913 <Patrivimwenge@gmail.com>
2024-11-27 22:37:45 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -5,14 +5,12 @@ def greet_programmer():
 
 def greet(name):
     print(f"Hello, {name}!")
-greet("Guido")
 
 def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
-greet_with_default()
+
 def add(num1, num2):
     return num1 + num2
 
 def halve(number):
-    return number/2
-
+    return number / 2
